c153147e196aa749e588bbdcd10f8107cb0bf5c3
AlexHew <alex@alphapod.com>
2016-11-06 03:56:08 +0800

    Add shape to json converter

---
+++ Assets/Scripts/Shape.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Shape {
+	public bool[] blocks;
+	/// <summary>
+	/// Need dimensions because single array does not have width etc length
+	/// </summary>
+	public int width;
+	public int depth;
+	public int height;
+
+	public override bool Equals(System.Object obj)
+	{
+		if (obj == null) { return false; }
+
+		Shape other = obj as Shape;
+
+		bool isEqual = (this.blocks.Length == other.blocks.Length)
+			&& this.width == other.width && this.depth == other.depth
+			&& this.height == other.height;
+
+		if (isEqual)
+		{
+			for (int i = 0; i < this.blocks.Length; i++)
+			{
+				isEqual = this.blocks[i] == other.blocks[i];
+
+				if (!isEqual)
+				{
+					return false;
+				}
+			}
+		}
+
+		return isEqual;
+	}
+}
