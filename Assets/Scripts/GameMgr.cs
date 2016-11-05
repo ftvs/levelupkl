afd81878b46ee5c9d23bcf6d7c2dd082d6eb73e8
Joniel Ibasco <jlibasco@gmail.com>
2016-11-06 05:03:55 +0800

    Implement PubSub broker and GameMgr skeleton

---
+++ Assets/Scripts/GameMgr.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+using PubSub;
+
+public class GameMgr : MonoBehaviour
+{
+	static GameMgr _instance;
+	private PubSubBroker _pubsubMgr = new PubSubBroker();
+
+	void OnEnable ()
+    {
+		if (_instance == null) {
+			_instance = this;
+			DontDestroyOnLoad(_instance);
+		}
+	}
+	
+	public PubSubBroker GetPubSubBroker()
+    {
+		return _pubsubMgr;
+	}
+
+	static public GameMgr Instance
+    {
+		get { return _instance; }
+	}
+}
