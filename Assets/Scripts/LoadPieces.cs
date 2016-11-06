97dc88ceb9604fe1b9bafd86d4cfa9708d750e5c
AlexHew <alex@alphapod.com>
2016-11-06 15:41:49 +0800

    Change checker to use world position.Add 2x2x2 cube

---
+++ Assets/Scripts/LoadPieces.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadPieces : MonoBehaviour {
+	public string levelFolderName;
+
+	void OnEnable()
+	{
+		Object[] pieces = Resources.LoadAll("Prefabs/" + levelFolderName);
+
+		foreach (Object pieceObj in pieces)
+		{
+			GameObject piece = Instantiate(pieceObj) as GameObject;
+		}
+
+//		GameObject piece = Instantiate() as GameObject;
+
+		this.enabled = false;
+	}
+}
