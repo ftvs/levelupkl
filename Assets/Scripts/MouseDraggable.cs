c5e659e4aff29474d4e81aafe022466827526fba
AlexHew <alex@alphapod.com>
2016-11-07 11:07:54 +0800

    Rename GameLevel to TeddyHeadGameLevel

--- Assets/Scripts/MouseDraggable.cs
+++ Assets/Scripts/MouseDraggable.cs
@@ -38,12 +38,4 @@ public class MouseDraggable : MonoBehaviour {
 
 		mousePosition = Input.mousePosition;
 	}
-
-	void OnMouseDrag()
-	{
-		float distance_to_screen = MainCamera.WorldToScreenPoint(gameObject.transform.position).z;
-		Vector3 pos_move = MainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen ));
-		transform.position = new Vector3( pos_move.x, transform.position.y, pos_move.z );
-
-	}
 }
